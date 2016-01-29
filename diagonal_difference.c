92146327d58646ec2598ef5cf0c64a899c395e70
Md Shahbaz Alam <shahbazalam17feb@gmail.com>
2016-01-29 19:23:50 +0530

    Warmup | Diagonal Difference

--- diagonal_difference.c
+++ diagonal_difference.c
@@ -25,3 +25,22 @@ int main(){
         printf("%d",(sum_d2-sum_d1));
     return 0;
 }
+
+int main(){
+    int n; 
+    scanf("%d",&n);
+    int a[n][n],sum_d1=0,sum_d2=0,k=n;
+    for(int a_i = 0; a_i < n; a_i++){
+       for(int a_j = 0; a_j < n; a_j++){
+          sum_d1 += a[a_i][a_i];
+          sum_d2 += a[--k][a_i];
+       }
+    }   
+    if(sum_d1>sum_d2)
+        printf("%d",(sum_d1-sum_d2));
+    else
+        printf("%d",(sum_d2-sum_d1));
+    return 0;
+}
+
+
